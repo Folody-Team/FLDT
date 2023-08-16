1bbf523c809f92a73c0fb286cdb0a6478cdfbcca
VN Fosly <Admin@Admin.(none)>
2023-08-16 23:51:27 +0700

    #1 add blockchain (processing)

--- src/chain.rs
+++ src/chain.rs
@@ -1,15 +1,17 @@
 mod block;
 
+use any_vec::any_value::Unknown;
 use rand::{distributions::Alphanumeric, Rng};
 use crate::math::{sha256 as hash};
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+use block::{Block as Block};
 use block::{BlockData as Data};
 use block::{ChainFLD as FLD};
 use block::{providerData as Provider};
 
-pub impl FLD {
-  fn new() -> Self {
+impl FLD {
+  pub fn new() -> Self {
     Self { 
       blocks: vec![],
       provider:  Provider {
@@ -22,52 +24,53 @@ pub impl FLD {
     }
   }
 
-  fn hash2binary(hash: &[u8]) {
+  pub fn hash2binary(hash: &[u8]) {
 
   }
 
   pub fn initialization(&mut self, fromAddr: String, toAddr: String) {
-    let mut block: [Any; 2] = [
-      hash(
-        rand::thread_rng()
-          .sample_iter(&Alphanumeric)
-          .take(7)
-          .map(char::from)
-          .collect()
-      )
-    ];
-    let mut blockInside = HashMap::new();
+    let mut block: Vec<HashMap<String, Block>> = Vec::new();
+    let mut id: HashMap<String, Block> = HashMap::new();
+    id.insert(String::from("id"), Block::Id(hash(
+      rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(7)
+        .map(char::from)
+        .collect()
+    )));
+    block.push(id.clone());
+    let mut blockInside: HashMap<String, Block> = HashMap::new();
     let blockData = Data {
       fromAddr: fromAddr,
       toAddr: toAddr,
       amount: 0.0,
       transactionMsg: "".to_owned(),
       createdAt: SystemTime::now(),
-      verifiedBy: [],
-      providerData: self.provider
+      verifiedBy: [].to_vec(),
+      providerData: self.provider.clone()
     };
 
-    blockInside.insert(block[0].to_string().to_owned(), hash(
-      rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(7)
-        .map(char::from)
-        .collect()
-    ));
-    blockInside.insert("blockData".to_owned(), blockData);
-    block[1] = blockData;
+    match id.clone().get(&String::from("id")).unwrap() {
+      Block::Id(value) => {
+        blockInside.insert( String::from(&value.to_string()).to_owned(), Block::Id(hash(
+          rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect()
+        )));
+      },
+        Block::BlockData(_) => todo!(),
+    }
+    
+    blockInside.insert(String::from("blockData"), Block::BlockData(blockData));
+    
+    block.push(blockInside);
     self.blocks.push(block);
   }
   
-  pub fn isValid(&self, block: &[Any; 2], previous_block: &[Any; 2]) {
+  pub fn isValid(&self, block: &Vec<HashMap<String, Block>>, previous_block: &Vec<HashMap<String, Block>>) {
 
   }
-  pub fn tryAdd (&mut self, block: [Any; 2]) {
-    let latest = self.blocks.last().expect("there is at least one block");
-    if self.isValid(&block, latest) {
-        self.blocks.push(block);
-    } else {
-        error!("could not add block");
-    }
-  }
+
 }
--- src/chain/block.rs
+++ src/chain/block.rs
@@ -1,8 +1,19 @@
-pub struct ChainFLD {
-  pub blocks: Vec,
-pub(crate) provider: providerData,
+use std::{time::SystemTime, collections::HashMap};
+
+
+#[derive(Debug,Clone)]
+pub enum Block {
+  Id(String),
+  BlockData(BlockData)
 }
 
+
+pub struct ChainFLD {
+  pub blocks: Vec<Vec<HashMap<String, Block>>>,
+  pub(crate) provider: providerData
+}
+
+#[derive(Debug,Clone)]
 pub struct providerData {
   pub name: String,
   pub shortName: String,
@@ -11,12 +22,14 @@ pub struct providerData {
   pub provider: String,
 
 }
+
+#[derive(Debug,Clone)]
 pub struct BlockData {
   pub fromAddr: String,
   pub toAddr: String,
   pub amount: f32,
   pub transactionMsg: String,
-  pub createdAt: i64,
+  pub createdAt: SystemTime,
   pub verifiedBy: Vec<String>,
   pub providerData: providerData
 
